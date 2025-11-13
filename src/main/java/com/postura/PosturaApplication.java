00a5c96190a4da7951571f324bd356ab4df81859
moon4528 <166115965+moon4528@users.noreply.github.com>
2025-11-14 00:53:06 +0900

    feat: Initial project setup for Postura Backend

---
+++ src/main/java/com/postura/PosturaApplication.java
@@ -0,0 +1,13 @@
+package com.postura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postura/PosturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
