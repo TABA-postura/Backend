8d5f6ccfcae90405ae236c36c9b3879309062665
kangdongdyu <kangkg0067@gmail.com>
2025-11-18 11:57:54 +0900

    refactor:메인 애플리케이션 설정 및 정리
    - JPA Auditing 기능 활성화 (@EnableJpaAuditing)
    - 실행 확인을 위한 임시 디버그 코드 제거 및 최종 정리

--- src/main/java/com/postura/PosturaApplication.java
+++ src/main/java/com/postura/PosturaApplication.java
@@ -1,13 +1,19 @@
 package com.postura;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Arrays;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class PosturaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PosturaApplication.class, args);
 	}
-
 }
