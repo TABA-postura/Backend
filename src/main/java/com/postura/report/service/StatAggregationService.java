c9c5de6b8f050a1efdf98c60722b1cd779e6d154
moon4528 <166115965+moon4528@users.noreply.github.com>
2025-12-20 13:12:37 +0900

    refact: springframework transactional로 변경(readOnly를 위함)

--- src/main/java/com/postura/report/service/StatAggregationService.java
+++ src/main/java/com/postura/report/service/StatAggregationService.java
@@ -9,11 +9,11 @@ import com.postura.report.entity.AggregateStat;
 import com.postura.report.repository.AggregateStatRepository;
 import com.postura.user.entity.User;
 import com.postura.user.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
